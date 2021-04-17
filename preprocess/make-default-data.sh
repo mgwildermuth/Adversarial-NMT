d4d833e5057594ee720df109aa6806e3af1fd153
Matthew Wildermuth <mgw32@pitt.edu>
2021-04-16 23:09:46 -0400

    Erased changes

--- preprocess/make-default-data.sh
+++ preprocess/make-default-data.sh
@@ -16,8 +16,8 @@ python short-sentences.py iwslt14.tokenized.de-en de-en 30
 
 # run the preprocessing scripts (creating lang dictionaries (-v 30000 is the max length of the list of vocab)) on the data
 echo running preprocess scripts
-python preprocess.py -d ../data/iwslt14.tokenized.de-en/vocab.en.pkl -v 30000 -b binarized_text.en.pkl -p ../data/iwslt14.tokenized.de-en/*.en
-python preprocess.py -d ../data/iwslt14.tokenized.de-en/vocab.de.pkl -v 30000 -b binarized_text.de.pkl -p ../data/iwslt14.tokenized.de-en/*.de
+python preprocess.py -d ../data/iwslt14.tokenized.de-en/vocab.en.pkl -v 30000 -b binarized_text.en.pkl ../data/iwslt14.tokenized.de-en/*.en
+python preprocess.py -d ../data/iwslt14.tokenized.de-en/vocab.de.pkl -v 30000 -b binarized_text.de.pkl ../data/iwslt14.tokenized.de-en/*.de
 
 # convert the pickeled vocab.*.pkl to dict.*.txt in the iwslt data folder
 echo running pickle-to-dict.py
